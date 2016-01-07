c1a11e18ef0c37552378c31565f9d0049afada1b
rohany <rohany@andrew.cmu.edu>
2016-01-06 22:08:50 -0500

    Loading Screen + merge test

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class LoadOnClick : MonoBehaviour {
 
+    public GameObject loadingImage;
 	public void LoadScene(int level)
     {
+        loadingImage.SetActive(true);
         Application.LoadLevel(level);
     }
 }
